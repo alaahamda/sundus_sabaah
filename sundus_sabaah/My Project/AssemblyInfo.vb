a12407dc7d15eb8ae76f92a5b98e9286dad14113
alaahamda <nor_alhaya@hotmail.com>
2013-05-19 12:48:03 +0300

    sundus
    hi

---
+++ sundus_sabaah/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("sundus_sabaah")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("sundus_sabaah")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9b1009b9-f762-49ae-988d-609a27c7b927")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ sundus_sabaah/home.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="home.aspx.vb" Inherits="sundus_sabaah.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:CheckBox ID="CheckBox1" runat="server" />
+        <asp:CheckBox ID="CheckBox2" runat="server" />
+    
+    </div>
+        <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </form>
+</body>
+</html>
